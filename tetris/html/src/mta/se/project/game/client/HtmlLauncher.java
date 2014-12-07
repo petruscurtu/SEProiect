71672546be8e99149e61590d299a0d04cb6f5ec6
petruscurtu <petruscurtu@yahoo.com>
2014-12-07 21:18:41 +0200

    Gdx demo
    Acesta este doar un demo pentru gdx. Proiectele pentru web si desktop
    merg. Pentru android vom vedea daca facem. Am si un proiect in care am
    integrat si partea de joc. Merge pe desktop dar crapa pe web. Mai
    trebuie sa lucrez la el. V-as ruga sa incercati sa le rulati sa vedeti
    daca merge.

---
+++ tetris/html/src/mta/se/project/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package mta.se.project.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import mta.se.project.game.tetris;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new tetris();
+        }
+}
